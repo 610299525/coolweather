24dc29d74381d8a783eb37f10088c327258c809b
610299525 <610299525@qq.com>
2018-08-07 16:39:00 +0800

    加入显示天气信息的功能。

--- app/src/main/java/www/wocaring/com/coolweather/util/Utility.java
+++ app/src/main/java/www/wocaring/com/coolweather/util/Utility.java
@@ -2,6 +2,8 @@ package www.wocaring.com.coolweather.util;
 
 import android.text.TextUtils;
 
+import com.google.gson.Gson;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -9,6 +11,7 @@ import org.json.JSONObject;
 import www.wocaring.com.coolweather.db.City;
 import www.wocaring.com.coolweather.db.County;
 import www.wocaring.com.coolweather.db.Province;
+import www.wocaring.com.coolweather.gson.Weather;
 
 public class Utility {
 
@@ -83,5 +86,21 @@ public class Utility {
             }
         }
         return false;
+    }
+
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
